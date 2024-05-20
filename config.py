444564249c85eaa752565c2847cc5925c3685de5
13Voland666 <lor.darkof@yandex.ru>
2024-05-20 13:45:17 +0300

    регистрация 1.1

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret_key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('X:\Politex\jukov///app.db') or \
-        'sqlite:///app.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
